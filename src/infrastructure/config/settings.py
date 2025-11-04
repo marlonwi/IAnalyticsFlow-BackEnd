2e56a1c376c2cc3655ccda6c956dfb4411fe95fb
Marlon William <marlonwijr2001@gmail.com>
2025-11-04 11:54:02 -0300

    Refatoração de config

--- src/infrastructure/config/settings.py
+++ src/infrastructure/config/settings.py
@@ -1,11 +1,11 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    openai_api_key: str = "" #Adicionar a key posteriormente
+    openai_api_key: str = "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" #Adicionar a key posteriormente
     db_host: str = "localhost"
     db_name: str = "challenge_db"
     db_user: str = "postgres"
-    db_password: str = "" #Adicionar a senha posteriormente
+    db_password: str = "123456" #Adicionar a senha posteriormente
     db_port: int = 5432
 
     class Config:
