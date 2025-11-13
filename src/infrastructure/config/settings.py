4d9a44016d7922155c3b3e4ca99b6d5f0bb69937
Marlon William <110416849+marlonwi@users.noreply.github.com>
2025-11-13 16:56:39 -0300

    Update settings.py

--- src/infrastructure/config/settings.py
+++ src/infrastructure/config/settings.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    openai_api_key: str = "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" #Adicionar a key posteriormente
+    openai_api_key: str = "" #Adicionar a key posteriormente
     db_host: str = "localhost"
     db_name: str = "challenge_db"
     db_user: str = "postgres"
